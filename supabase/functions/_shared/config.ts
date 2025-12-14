bb793e4e71af1c0fb1a3ab88c22637f082fdd29a
nerdcuba <nerdcuba@gmail.com>
2025-12-14 03:54:45 +0100

    chore: update dlocal go config to production

--- supabase/functions/_shared/config.ts
+++ supabase/functions/_shared/config.ts
@@ -1,8 +1,9 @@
 export const config = {
-    DLOCALGO_API_KEY: 'TWknwsAtJgazlDsawRIrCYLFJpJNOYMR',
-    DLOCALGO_SECRET_KEY: 'ApizLughS0RbihFquK651wpQoJmK9rJUpagMrUBg',
-    DLOCALGO_API_URL: 'https://api-sbx.dlocalgo.com',
-    PUBLIC_URL: 'https://ondai.ai',
-    SUPABASE_URL: 'https://supabase.ondai.ai',
-    SUPABASE_SERVICE_ROLE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic2VydmljZV9yb2xlIiwiaXNzIjoic3VwYWJhc2UiLCJpYXQiOjE3NTQzMzc2MDAsImV4cCI6MTkxMjEwNDAwMH0.ApXS8AfpwyHOtDmkrge18dFoBUZCnJIpSD7xF8O8IeQ'
+    DLOCALGO_API_KEY: Deno.env.get('DLOCALGO_API_KEY') || 'eYyxWqcFvMoYDiMIwdyLhQZRERseoYOs',
+    DLOCALGO_SECRET_KEY: Deno.env.get('DLOCALGO_SECRET_KEY') || 'IZ5bAeH4XS2v3oNsC6pgBAvTjHngeOVdbGUk1MDP',
+    DLOCALGO_API_URL: Deno.env.get('DLOCALGO_API_URL') || 'https://api.dlocalgo.com',
+    SMARTFIELDS_API_KEY: Deno.env.get('SMARTFIELDS_API_KEY') || '655c868e-d6a3-11f0-b386-0affcf7ce151',
+    PUBLIC_URL: Deno.env.get('PUBLIC_URL') || 'https://ondai.ai',
+    SUPABASE_URL: Deno.env.get('SUPABASE_URL') || 'https://supabase.ondai.ai',
+    SUPABASE_SERVICE_ROLE_KEY: Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic2VydmljZV9yb2xlIiwiaXNzIjoic3VwYWJhc2UiLCJpYXQiOjE3NTQzMzc2MDAsImV4cCI6MTkxMjEwNDAwMH0.ApXS8AfpwyHOtDmkrge18dFoBUZCnJIpSD7xF8O8IeQ'
 };
